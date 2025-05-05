c642f95d12c5096b7c13061ccf297bc18cc5d8b7
Dmitry Chizhov <chizhov.dee@gmail.com>
2025-05-05 20:26:08 +0300

    add 1,2 tasks solution for 3 lessons

---
+++ 3/2.task.py
@@ -0,0 +1,9 @@
+print("Введите стадии развития человека по порядку:")
+
+stage1 = input("Стадия 1: ")
+stage2 = input("Стадия 2: ")
+stage3 = input("Стадия 3: ")
+stage4 = input("Стадия 4: ")
+stage5 = input("Стадия 5: ")
+
+print(stage1, stage2, stage3, stage4, stage5, sep=" => ")
